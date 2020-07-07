e399555f179c42b7868fdb8d0ced343dee5cd921
Sergio A <Sergeagui@gmail.com>
2020-07-07 15:34:40 -0500

    first response

--- OmniMistressBot/Program.cs
+++ OmniMistressBot/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
 
 namespace OmniMistressBot
 {
     class Program
     {
+        static DiscordClient discord;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                //Regenerated Token
+                Token = "",
+                TokenType = TokenType.Bot
+            });
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }

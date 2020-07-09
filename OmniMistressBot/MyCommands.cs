a4d0de252e914fedcf07387abfd6d542aa67fff2
Sergio A <Sergeagui@gmail.com>
2020-07-09 15:46:01 -0500

    added pepe command

--- OmniMistressBot/MyCommands.cs
+++ OmniMistressBot/MyCommands.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 namespace OmniMistressBot
 {
@@ -14,7 +15,20 @@ namespace OmniMistressBot
         [Command("hi")]
         public async Task Hi(CommandContext ctx)
         {
-            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}");
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}. That'll be $3.50.");
+        }
+
+        [Command("pepe"), Aliases("feelsbad"), Description("Feels bad, man")]
+        public async Task Pepe(CommandContext context)
+        {
+            await context.TriggerTypingAsync();
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Pepe",
+                ImageUrl = "http://i.imgur.com/44SoSqS.jpg"
+            };
+            await context.RespondAsync(embed: embed);
         }
     }
 }

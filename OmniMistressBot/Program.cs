aed46dcaa7587fd36ee94281a097228d738308cf
Sergio A <Sergeagui@gmail.com>
2020-07-09 14:10:26 -0500

    expanded logs

--- OmniMistressBot/Program.cs
+++ OmniMistressBot/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
 using System;
 using System.Threading.Tasks;
 
@@ -54,23 +56,39 @@ namespace OmniMistressBot
 
         private static Task Discord_Ready(DSharpPlus.EventArgs.ReadyEventArgs e)
         {
-            throw new NotImplementedException();
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "Mistress", "Ready = Yes", DateTime.Now);
+            return Task.CompletedTask;
         }
         private static Task Discord_GuildAvailable(DSharpPlus.EventArgs.GuildCreateEventArgs e)
         {
-            throw new NotImplementedException();
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "Mistress", $"Guild available: {e.Guild.Name}", DateTime.Now);
+            return Task.CompletedTask;
         }
         private static Task Discord_ClientErrored(DSharpPlus.EventArgs.ClientErrorEventArgs e)
         {
-            throw new NotImplementedException();
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, "Mistress", $"Exception found: {e.Exception.GetType()}", DateTime.Now);
+            return Task.CompletedTask;
         }
-        private static Task Commands_CommandErrored(CommandErrorEventArgs e)
+        private static async Task Commands_CommandErrored(CommandErrorEventArgs e)
         {
-            throw new NotImplementedException();
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "Mistress", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+            if (e.Exception is ChecksFailedException ex)
+            {
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access Denied",
+                    Description = $"{emoji} You don't have permission to execute this command",
+                    Color = new DiscordColor(0xFF0000)
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
         }
         private static Task Commands_CommandExecuted(CommandExecutionEventArgs e)
         {
-            throw new NotImplementedException();
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "Mistress", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}", DateTime.Now);
+            return Task.CompletedTask;
         }
     }
 }

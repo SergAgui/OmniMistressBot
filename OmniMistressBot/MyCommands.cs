09e622269f7d0f755dd919b679fa270d882a00af
Sergio A <Sergeagui@gmail.com>
2020-07-09 07:57:03 -0500

    greeting command

---
+++ OmniMistressBot/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace OmniMistressBot
+{
+    public class MyCommands
+    {
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}");
+        }
+    }
+}
--- OmniMistressBot/Program.cs
+++ OmniMistressBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using System;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace OmniMistressBot
     class Program
     {
         static DiscordClient discord;
+        static CommandsNextModule commands;
         static void Main(string[] args)
         {
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
@@ -15,15 +17,26 @@ namespace OmniMistressBot
         {
             discord = new DiscordClient(new DiscordConfiguration
             {
-                //Regenerated Token
-                Token = "",
-                TokenType = TokenType.Bot
+                //Configuration of Discord Client
+                Token = "NzMwMTMwNTMwNjYxODI2Nzky.XwXMtw.TFI7XPTSlSIbffhZUyfyjNNQjtc",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
             });
+            //Respond to "ping" with "pong"
             discord.MessageCreated += async e =>
             {
                 if (e.Message.Content.ToLower().StartsWith("ping"))
-                    await e.Message.RespondAsync("pong!");
+                    await e.Message.RespondAsync(":ping_pong:");
             };
+            //Set prefix to commands
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!"
+            });
+            //Using MyCommands class to hold potential Commands
+            commands.RegisterCommands<MyCommands>();
+
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }

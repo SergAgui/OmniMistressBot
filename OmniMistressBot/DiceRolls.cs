d0de9a3404addaee91f14c05f65d095916e4454b
Sergio A <Sergeagui@gmail.com>
2020-07-13 18:46:40 -0500

    changed dice command

--- OmniMistressBot/DiceRolls.cs
+++ OmniMistressBot/DiceRolls.cs
@@ -11,7 +11,7 @@ namespace OmniMistressBot
 {
     public class DiceRolls
     {
-        [Command("rolld4"), Description("Rolls a d4 die")]
+        [Command("d4"), Description("Rolls a d4 die")]
         public async Task Roll4(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -20,7 +20,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 4);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld6"), Description("Rolls a d6 die")]
+        [Command("d6"), Description("Rolls a d6 die")]
         public async Task Roll6(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -29,7 +29,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 6);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld8"), Description("Rolls a d8 die")]
+        [Command("d8"), Description("Rolls a d8 die")]
         public async Task Roll8(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -38,7 +38,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 8);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld10"), Description("Rolls a d10 die")]
+        [Command("d10"), Description("Rolls a d10 die")]
         public async Task Roll10(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -47,7 +47,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 10);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld12"), Description("Rolls a d12 die")]
+        [Command("d12"), Description("Rolls a d12 die")]
         public async Task Roll12(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -56,7 +56,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 12);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld20"), Description("Rolls a d20 die")]
+        [Command("d20"), Description("Rolls a d20 die")]
         public async Task Roll20(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -65,7 +65,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 20);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld50"), Description("Rolls a d50 die")]
+        [Command("d50"), Description("Rolls a d50 die")]
         public async Task Roll50(CommandContext context)
         {
             await context.TriggerTypingAsync();
@@ -74,7 +74,7 @@ namespace OmniMistressBot
             int die = random.Next(1, 50);
             await context.RespondAsync($"Rolled a {die}");
         }
-        [Command("rolld100"), Description("Rolls a d100 die")]
+        [Command("d100"), Description("Rolls a d100 die")]
         public async Task Roll100(CommandContext context)
         {
             await context.TriggerTypingAsync();

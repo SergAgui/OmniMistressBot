11bd21ba4364d3fa5e65de7539a3755afd82fdc2
Sergio A <Sergeagui@gmail.com>
2020-07-15 15:32:20 -0500

    condensed dice roller to one method

--- OmniMistressBot/DiceRolls.cs
+++ OmniMistressBot/DiceRolls.cs
@@ -1,107 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
-using DSharpPlus.Interactivity;
+
 
 namespace OmniMistressBot
 {
     public class DiceRolls
     {
-        [Command("roll"), Description("Rolls any number of dice of any size")]
-        public async Task Roll(CommandContext context)
+        [Command("roll"), Aliases("r"), Description("Rolls any size die a given number of times")]
+        public async Task Roll(CommandContext context, string dice)
         {
-            //InteractivityModule interactivity = context.Client.GetInteractivityModule();
             await context.TriggerTypingAsync();
-            int[] com = new int[2];
-            string message = context.Message.Content;
-            string[] amtSize =  message.Split('d');
-            for (int i = 0; i < amtSize.Length; i++)
+
+            string[] amtSize = dice.Split('d');
+            int amountOfDice = Convert.ToInt32(amtSize[0]);
+            string[] rolls = new string[amountOfDice];
+
+            Random random = new Random();
+            for (int i = 0; i < amountOfDice; i++)
             {
-                com[i] = Convert.ToInt32(amtSize[i]);
+                rolls[i] = Convert.ToString(random.Next(1, Convert.ToInt32(amtSize[1])));
             }
-            
-            
-            Random random = new Random();
-            int die = random.Next();
-            
-        }
 
-        [Command("d4"), Description("Rolls a d4 die")]
-        public async Task Roll4(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
+            int sum = Array.ConvertAll(rolls, r => Convert.ToInt32(r)).Sum();
 
-            Random random = new Random();
-            int die = random.Next(1, 4);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d6"), Description("Rolls a d6 die")]
-        public async Task Roll6(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 6);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d8"), Description("Rolls a d8 die")]
-        public async Task Roll8(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 8);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d10"), Description("Rolls a d10 die")]
-        public async Task Roll10(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 10);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d12"), Description("Rolls a d12 die")]
-        public async Task Roll12(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 12);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d20"), Description("Rolls a d20 die")]
-        public async Task Roll20(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 20);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d50"), Description("Rolls a d50 die")]
-        public async Task Roll50(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 50);
-            await context.RespondAsync($"Rolled a {die}");
-        }
-        [Command("d100"), Description("Rolls a d100 die")]
-        public async Task Roll100(CommandContext context)
-        {
-            await context.TriggerTypingAsync();
-
-            Random random = new Random();
-            int die = random.Next(1, 100);
-            await context.RespondAsync($"Rolled a {die}");
+            await context.RespondAsync($"Rolls: [{String.Join(", ", rolls)}] Sum: {sum}");
         }
     }
 }

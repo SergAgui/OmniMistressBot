837eab8237eab934a5e21a423f9ec80f535e66a1
Sergio A <Sergeagui@gmail.com>
2020-07-16 15:41:57 -0500

    working to use reactions as responses

--- OmniMistressBot/DiceRolls.cs
+++ OmniMistressBot/DiceRolls.cs
@@ -9,6 +9,8 @@ using System.Xml.XPath;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
 
 
 namespace OmniMistressBot
@@ -33,6 +35,19 @@ namespace OmniMistressBot
             int sum = Array.ConvertAll(rolls, r => Convert.ToInt32(r)).Sum();
 
             await context.RespondAsync($"Rolls: [{String.Join(", ", rolls)}] Sum: {sum}");
+        }
+        [Command("roleoff"), Aliases("rc", "ro"), Description("Challenge another user to a roll off, highest roll upgrades role")]
+        public async Task RoleRoll(CommandContext context, DiscordUser user)
+        {
+            InteractivityModule interactivity = context.Client.GetInteractivityModule();
+            await context.TriggerTypingAsync();
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = $"{context.Message.Author} has challenged {user} to a Role Off! Do you accept?",
+                Description = "React with Yes :+1: || No :-1:"
+            };
+            var response = await context.RespondAsync(embed: embed);
         }
     }
 }

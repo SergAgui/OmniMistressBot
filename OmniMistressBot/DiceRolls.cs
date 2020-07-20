a1ae8de1b968278b957d33b92f3c9b8aaba352c2
Sergio A <Sergeagui@gmail.com>
2020-07-20 16:12:27 -0500

    Specific reactions are accepted now

--- OmniMistressBot/DiceRolls.cs
+++ OmniMistressBot/DiceRolls.cs
@@ -21,11 +21,13 @@ namespace OmniMistressBot
         public async Task Roll(CommandContext context, string dice)
         {
             await context.TriggerTypingAsync();
-
+            
+            //Spit string, ID values, and create array the size of the amount of dice being rolled
             string[] amtSize = dice.Split('d');
             int amountOfDice = Convert.ToInt32(amtSize[0]);
             string[] rolls = new string[amountOfDice];
-
+            
+            //Roll any size dice for 'i' times
             Random random = new Random();
             for (int i = 0; i < amountOfDice; i++)
             {
@@ -37,13 +39,15 @@ namespace OmniMistressBot
             await context.RespondAsync($"Rolls: [{String.Join(", ", rolls)}] Sum: {sum}");
         }
         
-        [Command("roleoff"), Aliases("rc", "ro"), Description("Challenge another user to a roll off, highest roll upgrades role")]
+        [Command("roleoff"), Aliases("rc", "ro"), Description("Challenge another user to a roll off, highest roll upgrades role! (ex. !rolloff @username)")]
         public async Task RoleRoll(CommandContext context, DiscordUser user)
         {
             InteractivityModule interactivity = context.Client.GetInteractivityModule();
             await context.TriggerTypingAsync();
 
-            DiscordEmoji emoji = DiscordEmoji.FromName(context.Client, ":+1:");
+            //Yes and No emojis
+            DiscordEmoji yes = DiscordEmoji.FromName(context.Client, ":+1:");
+            DiscordEmoji nope = DiscordEmoji.FromName(context.Client, ":-1:");
             var embed = new DiscordEmbedBuilder
             {
                 Title = $"{context.Message.Author.Username} has challenged {user.Username} to a Role Off! Do you accept?",
@@ -51,10 +55,15 @@ namespace OmniMistressBot
             };
             await context.RespondAsync(embed: embed);
 
-            var emote = await interactivity.WaitForReactionAsync(e => e == emoji, user, TimeSpan.FromSeconds(30));
-            if (emote != null)
+            //
+            var emote = await interactivity.WaitForReactionAsync(e => e == e.Name, user, TimeSpan.FromSeconds(30));
+            if (emote.Emoji.Name == yes)
             {
                 await context.RespondAsync($"{user.Username} has agreed! Role Off begins!!");
+            }
+            else if (emote.Emoji.Name == nope || emote == null)
+            {
+                await context.RespondAsync("Tough cookies :scream: They said NOPE!!");
             }
         }
          

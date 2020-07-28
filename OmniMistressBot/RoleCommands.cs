117ab19b276699bf737009be60064aed45dd479a
Sergio A <Sergeagui@gmail.com>
2020-07-28 16:38:45 -0500

    Roles are checked before granted

--- OmniMistressBot/RoleCommands.cs
+++ OmniMistressBot/RoleCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Net.Mime;
 using System.Runtime.InteropServices;
@@ -21,9 +22,30 @@ namespace OmniMistressBot
         [Hidden]
         public async Task UpgradeRole(CommandContext context, DiscordMember member, string role)
         {
-            var upgradeRole = context.Guild.Roles.FirstOrDefault(x => x.Name == role);
-            await member.GrantRoleAsync(upgradeRole);
-            await context.RespondAsync($"Updated to {upgradeRole.Name}");
+            //ReadOnlyList to just names
+            List<string> roleList = new List<string>();
+            var check = context.Guild.Roles;
+            foreach (var item in check)
+            {
+                roleList.Add(item.Name);
+            }
+
+            //check if role exists in server
+            if (member.IsBot != true && roleList.Exists(r => r == role))
+            {
+                var upgradeRole = context.Guild.Roles.FirstOrDefault(x => x.Name == role);
+                await member.GrantRoleAsync(upgradeRole);
+                await context.RespondAsync($"Updated to {upgradeRole.Name}");
+            }
+            else
+            {
+                await context.RespondAsync($"Couldn't complete. The {role} role does not exist in this server.");
+            }
         }
+
+        /*public async Task DowngradeRole(CommandContext context, DiscordMember member, string role)
+        {
+
+        }*/
     }
 }

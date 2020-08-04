eb82b4a8c01af68e8e3d1d644cc3974194964f9a
Sergio A <Sergeagui@gmail.com>
2020-08-04 14:29:48 -0500

    starting voice commands for music

--- OmniMistressBot/Program.cs
+++ OmniMistressBot/Program.cs
@@ -4,6 +4,8 @@ using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.CommandsNext.Exceptions;
 using DSharpPlus.Entities;
 using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
+using DSharpPlus.VoiceNext.Codec;
 using System;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -15,6 +17,7 @@ namespace OmniMistressBot
         static DiscordClient discord;
         static CommandsNextModule commands;
         static InteractivityModule interactivity;
+        static VoiceNextClient voice;
         
         static void Main(string[] args)
         {
@@ -45,11 +48,18 @@ namespace OmniMistressBot
                 Timeout = TimeSpan.FromMinutes(2)
             });
 
+            //Voice channel will be used for broadcasting music
+            voice = discord.UseVoiceNext(new VoiceNextConfiguration
+            {
+                VoiceApplication = VoiceApplication.Music,
+                EnableIncoming = false
+            });
+
             //Set prefix to commands
             commands = discord.UseCommandsNext(new CommandsNextConfiguration
             {
                 StringPrefix = "!",
-                EnableDms = true,
+                EnableDms = false,
                 EnableMentionPrefix = true
             });
 
@@ -62,6 +72,7 @@ namespace OmniMistressBot
             commands.RegisterCommands<InteractiveCommands>();
             commands.RegisterCommands<DiceRolls>();
             commands.RegisterCommands<RoleCommands>();
+            commands.RegisterCommands<MusicCommands>();
 
             await discord.ConnectAsync();
             await Task.Delay(-1);

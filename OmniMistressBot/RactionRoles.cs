ccf54aba0fbf5252ac96197d3aee9372847f1ae2
Sergio <Aguilar>
2020-10-31 15:53:31 -0500

    Added the command call

--- OmniMistressBot/RactionRoles.cs
+++ OmniMistressBot/RactionRoles.cs
@@ -4,12 +4,14 @@ using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 
 namespace OmniMistressBot
 {
     class RactionRoles
     {
+        [Command("newroll"), Aliases("nr"), Description("Create a new roll. [!newrole {RoleName} {Color} {Permissions (can be left blank)} {Mentionable (can be left blank} {Reason (can be left blank)}]")]
         public async Task CreateRoll(CommandContext context, string name, DiscordColor color, Permissions? permissions = null, bool mentionable = true, string reason = null)
         {
             InteractiveCommands interactiveCommands = new InteractiveCommands();
@@ -18,6 +20,7 @@ namespace OmniMistressBot
             await context.RespondAsync($"Role {name} has been created. Color = {color} | Mentionable = {mentionable} | Permissions (if any) = {permissions}");
         }
 
+        [Command("rolecolors"), Description("Displays possible colors that can be used for Roles")]
         public async Task Colors(CommandContext context)
         {
             await context.TriggerTypingAsync();
